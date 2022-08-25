db0c1f8f4e4f094eab7ac83deb4e97cbe7512dd2
Aidan Juma <aidan@aidanjuma.dev>
2022-08-25 11:24:58 +0200

    🗂 project restructure, added tailwindcss

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const Home = () => (
+  <>
+    <Head>
+      <title>天国 • Anime & Manga</title>
+      <meta
+        name="description"
+        content="Find your favourite Japanese media on 天国 (tengoku)! Built with Consumet, Next & React."
+      />
+    </Head>
+  </>
+);
+
+export default Home;
