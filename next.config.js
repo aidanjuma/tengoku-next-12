de6cd472d96401cfa09f34c796ac3403b219366f
Aidan Juma <aidan@aidanjuma.dev>
2022-08-28 23:56:39 +0300

    🧹 (chores) added consumet, image domain...
    TODO: FeaturedContent component!

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    domains: ["s4.anilist.co"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ src/components/home/FeaturedContent.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// TODO: FeauturedContent Component (Home, Manual Update)
+const FeaturedContent = () => (
+  <section id="featured-content-section">
+    <div></div>
+  </section>
+);
+
+export default FeaturedContent;
