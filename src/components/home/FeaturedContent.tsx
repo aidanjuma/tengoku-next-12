f4e64f9c5affd2b67abfdca05b01b1e544731b39
Aidan Juma <aidan@aidanjuma.dev>
2022-08-30 20:14:31 +0300

    📝 added featured content description*
    *tentative description layout for mobile.
    
    next TODO: add trending & popular anime sliders underneath featured.

--- src/components/home/FeaturedContent.tsx
+++ src/components/home/FeaturedContent.tsx
@@ -10,10 +10,10 @@ const FeaturedContent = () => (
           "url(https://s4.anilist.co/file/anilistcdn/media/anime/banner/133844-uIaUmh5aJX3M.jpg)",
       }}
     ></div>
-    <div className="grid w-full h-under-banner px-6">
-      <div className="mt-[-110px]">
+    <div className="grid w-full h-under-banner px-5">
+      <div className="mt-[-190px] lg:mt-[-250px]">
         <div className="grid grid-cols-2 grid-rows-1">
-          <div className="w-[130px] h-[184px] md:w-[150px] md:h-[212px] lg:w-[200px] lg:h-[282px] relative">
+          <div className="w-[160px] h-[226px] lg:w-[200px] lg:h-[282px] relative">
             <Image
               src="https://s4.anilist.co/file/anilistcdn/media/anime/cover/large/bx133844-E32FjKZ0XxEs.jpg"
               alt="Overlord IV's album image."
@@ -21,9 +21,32 @@ const FeaturedContent = () => (
               objectFit="cover"
             />
           </div>
-          <h2 className="font-lexend font-medium text-right text-3xl text-white">
-            Overlord IV
-          </h2>
+          <div className="text-right text-white">
+            <h2 className="font-lexend font-medium text-2xl pb-4">
+              Overlord IV
+            </h2>
+            <div className="h-32 lg:h-44 overflow-scroll">
+              <p className="font-dmsans font-normal text-base text-justify">
+                With his foundations now set in this new world, the first steps
+                of Ains Ooal Gown&apos;s master plan apparent begin to come to
+                fruition. The value of Carne village and especially the
+                political value of his alter ego Momon are reaffirmed; the
+                “hidden” genius of his actions is continuously met with shock
+                and awe by Demiurge, Albedo, the rest of his guardians, and even
+                Ains himself at his “great wisdom.” His attempts to act in a way
+                befitting the ruler of Nazarick, continuing to further cement
+                his guardian&apos;s loyalty, push him ever further down the path
+                of not-so-intentional world domination. Without human conscience
+                due to being undead, he is motivated only to continue searching
+                for other players from Yggdrasil and to gain power to better
+                protect the children and home of him and his forty former
+                companions. Lord Ains Ooal Gown maintains his mantle of Overlord
+                and leads the Great Tomb of Nazarick unto the world stage,
+                directly into a vicious power struggle between two great
+                empires.
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
