603e8180adc509c32390da0bebf67b810bb66164
Aidan Juma <aidan@aidanjuma.dev>
2022-09-08 23:17:49 +0100

    🗃️ (chore) added future info route starting point

---
+++ src/components/info/InfoCover.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const InfoCover = () => {};
+
+export default InfoCover;
---
+++ src/pages/info/[id].tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const InfoPage = () => {};
+
+export default InfoPage;
